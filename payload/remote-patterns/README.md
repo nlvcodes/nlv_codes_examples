5107541723bd8f5586a82851be07d505f2386b43
nick <nick0vogel@gmail.com>
2025-12-27 16:01:37 -0500

    init commit

---
+++ payload/remote-patterns/README.md
@@ -0,0 +1,1 @@
+# How to get images to show up
---
+++ payload/remote-patterns/src/payload.config.ts
@@ -0,0 +1,51 @@
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media'
+import { s3Storage } from '@payloadcms/storage-s3'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URL || '',
+  }),
+  sharp,
+  plugins: [
+    s3Storage({
+      collections: {
+        media: {
+          generateFileURL: ({ filename }) => `https://images.nlvcodes.com/remote-patterns/${filename}`,
+        },
+      },
+      bucket: process.env.S3_BUCKET!,
+      config: {
+        endpoint: process.env.S3_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
+        region: 'auto',
+        forcePathStyle: true,
+      },
+    }),
+  ],
+})
